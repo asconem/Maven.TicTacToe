85177829316897c537c5bf58bf0d9e87adbaa20a
asconem <asconem@gmail.com>
2020-02-12 21:37:18 -0500

    In progress.

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,11 +4,23 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+    private Character [] [] board;
+
     public Board(Character[][] matrix) {
+        this.board = matrix;
     }
 
     public Boolean isInFavorOfX() {
-        return null;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][0] == 'X' && board[i][1] == 'X' && board[i][2] == 'X') {
+                return true;
+            }
+            if (board[0][i] == 'X' && board[1][i] == 'X' && board[2][i] == 'X') {
+                return true;
+            }
+
+        }
+        if (board[0][0])
     }
 
     public Boolean isInFavorOfO() {
